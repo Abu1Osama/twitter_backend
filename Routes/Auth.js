0a8d6dd749bb6400b121f40a83a5926d64e9b687
Abu Osama <112826965+Abu1Osama@users.noreply.github.com>
2023-08-22 14:13:37 +0530

    userid

--- Routes/Auth.js
+++ Routes/Auth.js
@@ -40,8 +40,17 @@ router.post('/login', async (req, res) => {
       return res.status(401).json({ error: 'Authentication failed' });
     }
     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    const userResponse = {
+      token,
+      username: user.username,
+      name: user.name,
+      dateOfBirth: user.dateOfBirth,
+      userId: user._id,
+      followers: user.followers,
+    };
+    res.status(200).json(userResponse);
 
-    res.status(200).json({ token, username: user.username, name: user.name ,dateOfBirth:user.dateOfBirth,userId: user._id }); 
+    // res.status(200).json({ token, username: user.username, name: user.name ,dateOfBirth:user.dateOfBirth,userId: user._id }); 
     await user.save();
     
  
