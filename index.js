2ac9cb30f0adf17d95b053cb11123c3e7fe69ca4
Abu Osama <112826965+Abu1Osama@users.noreply.github.com>
2023-08-28 15:31:27 +0530

    crypto

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const storage = multer.diskStorage({
     cb(null, "uploads/");
   },
   filename: (req, file, cb) => {
-    cb(null, Date.now() + "-" + file.originalname);
+    cb(null, Date.now() + "-" + file.originalname); 
   },
   fileFilter: (req, file, cb) => {
     if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
