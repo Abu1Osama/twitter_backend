1c702165ea9e693ca8fa3751381239aa4a9b7b0c
Abu Osama <112826965+Abu1Osama@users.noreply.github.com>
2023-09-01 15:54:42 +0530

    done

--- Routes/Auth.js
+++ Routes/Auth.js
@@ -33,9 +33,80 @@ const avatarStorage = multer.diskStorage({
 });
 
 const uploadAvatar = multer({ storage: avatarStorage });
+// router.post("/register", uploadAvatar.single("avatar"), async (req, res) => {
+//   try {
+//     const { username, password, name, dateOfBirth } = req.body;
+
+//     const existingUser = await User.findOne({ username });
+//     if (existingUser) {
+//       return res.status(400).json({ error: "Username already exists" });
+//     }
+
+//     const hashedPassword = await bcrypt.hash(password, 10);
+
+//     const avatar = req.file ? req.file.filename : null;
+//     if (!avatar) {
+//       return res.status(400).json({ error: "Avatar upload failed" });
+//     }
+//     const user = new User({
+//       username,
+//       password: hashedPassword,
+//       name,
+//       dateOfBirth,
+//       avatar,
+//     });
+//     await user.save();
+
+//     res.status(201).json({ message: "User registered successfully" });
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500).json({ error: "Registration failed" });
+//   }
+// });
+
+// User login
+// router.post("/login", async (req, res) => {
+//   try {
+//     const { username, password } = req.body;
+//     const user = await User.findOne({ username });
+//     if (!user) {
+//       return res.status(401).json({ error: "Authentication failed" });
+//     }
+//     const validPassword = await bcrypt.compare(password, user.password);
+//     if (!validPassword) {
+//       return res.status(401).json({ error: "Authentication failed" });
+//     }
+//     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+//       expiresIn: "1h",
+//     });
+//     const userResponse = {
+//       token,
+//       username: user.username,
+//       name: user.name,
+//       dateOfBirth: user.dateOfBirth,
+//       userId: user._id,
+//       followers: user.followers,
+//       avatar: user.avatar,
+//     };
+//     res.status(200).json(userResponse);
+
+//     await user.save();
+//   } catch (error) {
+//     res.status(500).json({ error: "Authentication failed" });
+//   }
+// });
+
 router.post("/register", uploadAvatar.single("avatar"), async (req, res) => {
   try {
     const { username, password, name, dateOfBirth } = req.body;
+
+    if (!username.includes("@")) {
+      return res.status(400).json({ error: "Username must contain the '@' symbol" });
+    }
+
+    if (password.length < 6) {
+      return res.status(400).json({ error: "Password must be 6 characters or longer" });
+    }
 
     const existingUser = await User.findOne({ username });
     if (existingUser) {
@@ -64,18 +135,23 @@ router.post("/register", uploadAvatar.single("avatar"), async (req, res) => {
   }
 });
 
-// User login
 router.post("/login", async (req, res) => {
   try {
     const { username, password } = req.body;
     const user = await User.findOne({ username });
+
     if (!user) {
-      return res.status(401).json({ error: "Authentication failed" });
+      // User not found (Invalid Username)
+      return res.status(401).json({ error: "User not found" });
     }
+
     const validPassword = await bcrypt.compare(password, user.password);
     if (!validPassword) {
-      return res.status(401).json({ error: "Authentication failed" });
+      // Incorrect Password
+      return res.status(401).json({ error: "Incorrect password" });
     }
+
+    // Authentication successful, generate and return token
     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
       expiresIn: "1h",
     });
@@ -90,11 +166,13 @@ router.post("/login", async (req, res) => {
     };
     res.status(200).json(userResponse);
 
-    await user.save();
+    await user.save(); // Note: You might not need this line in the login route.
   } catch (error) {
+    // Generic server error (for unexpected errors)
     res.status(500).json({ error: "Authentication failed" });
   }
 });
+
 router.post("/logout", async (req, res) => {
   try {
     req.user.tokens = req.user.tokens.filter((token) => token !== req.token);
